6bb2ee5f490f83e0c766e5bc0ba76065515a7cf4
NagarajPatilM <nagarajpatilm95@gmail.com>
2019-12-19 10:39:37 +0530

    added pdf,png,txt

--- SMSAndEmail/SMSAndEmail/src/main/java/com/tyss/smsandmailservice/service/EmailService.java
+++ SMSAndEmail/SMSAndEmail/src/main/java/com/tyss/smsandmailservice/service/EmailService.java
@@ -12,5 +12,9 @@ public interface EmailService {
 
 	public Response sendEmail(String from, List<Email> tos, String subject, List<Email> ccs ,String content) throws IOException;
 
-	public Response sendEmail2(String from, List<Email> tos, String subject, List<Email> ccs ,String content,MultipartFile attachements)throws IOException;
+	//public Response sendEmail1(String from, List<Email> tos, String subject, List<Email> ccs ,String content, MultipartFile file)throws IOException;
+	
+	public Response sendEmail2(String from, String tos, String subject, String ccs ,String content, List<MultipartFile> file)throws Exception;
+	
+	public Response sendEmail3(String from, List<Email> tos, String subject, List<Email> ccs ,String content, List<MultipartFile> file)throws IOException;
 }
